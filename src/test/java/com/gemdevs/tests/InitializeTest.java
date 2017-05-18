19aca98a62d8c30968385cf243a818e3145f6a85
Victor Fleurant <victorfleurant@gmail.com>
2017-05-18 19:29:06 -0400

    Initialization of Euler Project to learn Test Driven Developpement in Java

---
+++ src/test/java/com/gemdevs/tests/InitializeTest.java
@@ -0,0 +1,14 @@
+package com.gemdevs.tests;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Test;
+
+public class InitializeTest {
+
+    @Test
+    public void initializationTest(){
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(softly).isNotNull();
+    }
+
+}
